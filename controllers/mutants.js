fee69857ed101ed07139d68bf10a432bc43db0fc
Luciano Masuero <luchimasuero@gmail.com>
2019-11-16 17:11:25 -0300

    1 - Basic controller and models structure

---
+++ controllers/mutants.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Cargamos los modelos para usarlos posteriormente
+var Mutant = require('../models/mutant');
+
+// Conseguir datos de un usuario
+
+function get(req, res){
+    var userId = req.params.id;
+
+}
---
+++ models/mutant.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Cargamos el módulo de mongoose
+var mongoose =  require('mongoose');
+
+// Usaremos los esquemas
+var Schema = mongoose.Schema;
+
+// Creamos el objeto del esquema y sus atributos
+var MutantSchema = Schema({
+
+});
+
+// Exportamos el modelo para usarlo en otros ficheros
+module.exports = mongoose.model('Mutant', MutantSchema);
