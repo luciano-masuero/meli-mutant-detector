35f9c870a48692a767e2730666e941714946a829
Luciano Masuero <luchimasuero@gmail.com>
2019-11-18 13:06:13 -0300

    3 - Create matrix and log

---
+++ helpers/utils.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { debug } = require('../config');
+
+
+/**
+ * Receives an array of strings and returns the
+ * matrix generated
+ *
+ * @param stringArray
+ * @returns {Array}
+ */
+module.exports.createMatrix = function(stringArray) {
+
+    var matrix = [];
+    var x = stringArray.length;
+    var y = stringArray[0].length;
+
+    for (var i = 0; i < x; i++) {
+        matrix[i] = [];
+        for (var j = 0; j < y; j++) {
+            matrix[i][j] = stringArray[i][j];
+        }
+    }
+
+    return matrix;
+};
+
+
+/**
+ * If we are on debug mode then log
+ *
+ * @param string
+ * @returns {Array}
+ */
+module.exports.log = function(string) {
+    if (debug) {
+        console.log(string);
+    }
+};
